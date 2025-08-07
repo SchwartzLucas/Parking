c25016e68953452747dd3c455188aa357706a0d6
SchwartzLucas <lucassdesouza10@gmail.com>
2025-08-07 00:33:46 -0300

    Parte inicial do programa que estou fazendo de um estacionamento

---
+++ Entities/Service/Estacionamento.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Entities.Service {
+    internal class Estacionamento {
+
+        public double PrecoInicial = 15.00;
+        public double PrecoHora = 4.00;
+        public List<Vehicle> Carro { get; set; }
+
+        public Estacionamento() { }
+        public Estacionamento(double precoInicial, double precoHora) {
+            PrecoInicial = precoInicial;
+            PrecoHora = precoHora;
+        }
+
+        public void AddCarro(Vehicle carro) {
+            Carro.Add(carro);
+        }
+
+        public void RemoveCarro(Vehicle carro) {
+            Carro.Remove(carro);
+        }
+
+        public void ListarCarros(List<Vehicle> carros) {
+            foreach (Vehicle carro in carros) {
+                Console.WriteLine(carro);
+            }
+        }
+
+        public double FinalPrice() {
+            double precoFinal = PrecoInicial * PrecoHora;
+            return precoFinal;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Parking.Entities.Service;
+
+try {
+
+
+
+
+    string path = @"C:\temp\placas\placacarros.txt";
+    using (StreamReader sr = new StreamReader(path)) {
+            string[] placas = File.ReadAllLines(path);
+
+        Estacionamento estacionamento = new Estacionamento();
+
+    }
+}catch (FormatException e) {
+    Console.WriteLine($"An format error ocurrer: {e.Message}");
+}
